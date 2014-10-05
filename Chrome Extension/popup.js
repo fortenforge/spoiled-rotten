e6c5cd61078cabe46232b04f4d95f4f845156859
Cyklotope <kevinzhu2006@gmail.com>
2014-10-05 05:31:29 -0400

    added titles

--- Chrome Extension/popup.js
+++ Chrome Extension/popup.js
@@ -8,6 +8,15 @@ function update() {
   });
 }
 
+titles = ['Winter Is Coming', 'The Kingsroad', 'Lord Snow', 'Cripples, Bastards, and Broken Things', 
+'The Wolf and the Lion', 'A Golden Crown', 'You Win or You Die', 'The Pointy End', 'Baelor', 'Fire and Blood', 
+'The North Remembers', 'The Night Lands', 'What Is Dead May Never Die', 'Garden of Bones', 'The Ghost of Harrenhal', 
+'The Old Gods and the New', 'A Man Without Honor', 'The Prince of Winterfell', 'Blackwater', 'Valar Morghulis', 
+'Valar Dohaeris', 'Dark Wings, Dark Words', 'Walk of Punishment', 'And Now His Watch Is Ended', 'Kissed by Fire', 
+'The Climb', 'The Bear and the Maiden Fair', 'Second Sons', 'The Rains of Castamere', 'Mhysa', 
+'Two Swords', 'The Lion and the Rose', 'Breaker of Chains', 'Oathkeeper', 'First of His Name', 
+'The Laws of Gods and Men', 'Mockingbird', 'The Mountain and the Viper', 'The Watchers on the Wall', 'The Children'];
+
 chrome.storage.sync.get('season', function (data) {
   if (chrome.runtime.lastError) {
     return;
@@ -48,6 +57,7 @@ chrome.storage.sync.get('episode', function (data) {
     return;
   }
   console.log('efull');
+  var link1 = document.getElementById('link1');
   var link2 = document.getElementById('link2');
   switch(data.episode)
   {
@@ -99,6 +109,7 @@ chrome.storage.sync.get('episode', function (data) {
       link2.value = '1';
   }
   document.getElementById('num2').innerHTML = 'Episode ' + link2.value;
+  document.getElementById('num3').innerHTML = titles[(parseInt(link1.value) - 1) * 10 + parseInt(link2.value) - 1];
   update();
 });
 /*console.log('hi');
@@ -111,8 +122,10 @@ chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 });*/
 document.addEventListener('DOMContentLoaded', function() {
   var link1 = document.getElementById('link1');
+  var link2 = document.getElementById('link2');
   link1.addEventListener('input', function() {
     document.getElementById('num1').innerHTML = 'Season ' + link1.value;
+    document.getElementById('num3').innerHTML = titles[(parseInt(link1.value) - 1) * 10 + parseInt(link2.value) - 1];
     console.log('link1');
     chrome.storage.sync.set({'season': link1.value}, function() {
       console.log('Setting for season updated');
@@ -121,9 +134,11 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 });
 document.addEventListener('DOMContentLoaded', function() {
+  var link1 = document.getElementById('link1');
   var link2 = document.getElementById('link2');
   link2.addEventListener('input', function() {
     document.getElementById('num2').innerHTML = 'Episode ' + link2.value;
+    document.getElementById('num3').innerHTML = titles[(parseInt(link1.value) - 1) * 10 + parseInt(link2.value) - 1];
     console.log('link2');
     chrome.storage.sync.set({'episode': link2.value}, function() {
       console.log('Setting for episode updated');
